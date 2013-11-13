2a338c2d30b96c6a90c1d56802662cbda4b5a3cd
Oscar Sweatman <oscarsweatman@derecho.io>
2013-11-12 21:10:33 -0600

    First pass unity project
    Adding initial unity project with a basic ship script to start with.  I
    have concerns about the project/Library folder and how it will act with
    github.

---
+++ UnityProject/TwoWeekAsteroids/Assets/Scripts/Ship.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour 
+{
+	public float WorldWidth = 10.0f;
+	public float WorldHeight = 10.0f;
+	
+	public float Speed = 5.0f;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f) * Speed * Time.deltaTime);
+		
+		Vector3 p = transform.position;
+		
+		float halfWidth = WorldWidth * 0.5f;
+		float halfHeight = WorldHeight * 0.5f;
+		
+		if (p.x > halfWidth)
+			p.x -= WorldWidth;
+		else if (p.x < -halfWidth)
+			p.x += WorldWidth;
+		
+		if (p.y > halfHeight)
+			p.y -= WorldHeight;
+		else if (p.y < -halfHeight)
+			p.y += WorldHeight;
+		
+		transform.position = p;
+	}
+	
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+		
+		float l = WorldWidth * -0.5f;
+		float r = -l;
+		float t = WorldHeight * 0.5f;
+		float b = -t;
+		
+		Gizmos.DrawLine(new Vector3(l, t, 0.0f), new Vector3(r, t, 0.0f));
+		Gizmos.DrawLine(new Vector3(r, t, 0.0f), new Vector3(r, b, 0.0f));
+		Gizmos.DrawLine(new Vector3(r, b, 0.0f), new Vector3(l, b, 0.0f));
+		Gizmos.DrawLine(new Vector3(l, b, 0.0f), new Vector3(l, t, 0.0f));
+	}
+}
